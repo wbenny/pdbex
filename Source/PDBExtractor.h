4515b4343366b2d30e75364b3c24826d39ef5cdf
Petr Benes <w.benny@outlook.com>
2019-01-10 22:27:53 +0100

    bump version

--- Source/PDBExtractor.h
+++ Source/PDBExtractor.h
@@ -8,9 +8,9 @@
 #include <string>
 
 #define PDBEX_VERSION_MAJOR 0
-#define PDBEX_VERSION_MINOR 10
+#define PDBEX_VERSION_MINOR 11
 
-#define PDBEX_VERSION_STRING "0.10"
+#define PDBEX_VERSION_STRING "0.11"
 
 class PDBExtractor
 {
