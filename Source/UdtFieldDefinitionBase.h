5c9320e2e0a5430c71ac5184a92bf7663a59b8f1
unknown <jon.smit@gmail.com>
2019-11-18 04:05:25 +0200

    code style

--- Source/UdtFieldDefinitionBase.h
+++ Source/UdtFieldDefinitionBase.h
@@ -5,154 +5,76 @@
 
 class UdtFieldDefinitionBase
 {
-	public:
-		virtual
-		void
-		VisitBaseType(
-			const SYMBOL* Symbol
-			)
-		{
+public:
+	virtual	void VisitBaseType(const SYMBOL* Symbol)
+	{
+	}
 
-		}
+	virtual void VisitTypedefTypeBegin(const SYMBOL* Symbol)
+	{
+	}
 
-		virtual
-		void
-		VisitTypedefTypeBegin(
-			const SYMBOL* Symbol
-		)
-		{
+	virtual	void VisitTypedefTypeEnd(const SYMBOL* Symbol)
+	{
+	}
 
-		}
+	virtual	void VisitEnumType(const SYMBOL* Symbol)
+	{
+	}
 
-		virtual
-		void
-		VisitTypedefTypeEnd(
-			const SYMBOL* Symbol
-			)
-		{
+	virtual	void VisitUdtType(const SYMBOL* Symbol)
+	{
+	}
 
-		}
+	virtual void VisitPointerTypeBegin(const SYMBOL* Symbol)
+	{
+	}
 
-		virtual
-		void
-		VisitEnumType(
-			const SYMBOL* Symbol
-			)
-		{
+	virtual	void VisitPointerTypeEnd(const SYMBOL* Symbol)
+	{
+	}
 
-		}
+	virtual	void VisitArrayTypeBegin(const SYMBOL* Symbol)
+	{
+	}
 
-		virtual
-		void
-		VisitUdtType(
-			const SYMBOL* Symbol
-			)
-		{
+	virtual	void VisitArrayTypeEnd(const SYMBOL* Symbol)
+	{
+	}
 
-		}
+	virtual	void VisitFunctionTypeBegin(const SYMBOL* Symbol)
+	{
 
-		virtual
-		void
-		VisitPointerTypeBegin(
-			const SYMBOL* Symbol
-		)
-		{
+	}
 
-		}
+	virtual	void VisitFunctionTypeEnd(const SYMBOL* Symbol)
+	{
+	}
 
-		virtual
-		void
-		VisitPointerTypeEnd(
-			const SYMBOL* Symbol
-			)
-		{
+	virtual	void VisitFunctionArgTypeBegin(const SYMBOL* Symbol)
+	{
+	}
 
-		}
-
-		virtual
-		void
-		VisitArrayTypeBegin(
-			const SYMBOL* Symbol
-		)
-		{
-
-		}
-
-		virtual
-		void
-		VisitArrayTypeEnd(
-			const SYMBOL* Symbol
-			)
-		{
-
-		}
-
-		virtual
-		void
-		VisitFunctionTypeBegin(
-			const SYMBOL* Symbol
-		)
-		{
-
-		}
-
-		virtual
-		void
-		VisitFunctionTypeEnd(
-			const SYMBOL* Symbol
-			)
-		{
-
-		}
-
-		virtual
-		void
-		VisitFunctionArgTypeBegin(
-			const SYMBOL* Symbol
-		)
-		{
-
-		}
-
-		virtual
-		void
-		VisitFunctionArgTypeEnd(
-			const SYMBOL* Symbol
-			)
-		{
-
-		}
+	virtual void VisitFunctionArgTypeEnd(const SYMBOL* Symbol)
+	{
+	}
 
 
-		virtual
-		void
-		SetMemberName(
-			const CHAR* MemberName
-		)
-		{
+	virtual	void SetMemberName(const CHAR* MemberName)
+	{
+	}
 
-		}
+	virtual	std::string GetPrintableDefinition() const
+	{
+		return std::string();
+	}
 
-		virtual
-		std::string
-		GetPrintableDefinition() const
-		{
-			return std::string();
-		}
+	virtual	void SetSettings(void* MemberDefinitionSettings)
+	{
+	}
 
-		virtual
-		void
-		SetSettings(
-			void* MemberDefinitionSettings
-			)
-		{
-
-		}
-
-		virtual
-		void*
-		GetSettings()
-		{
-			return nullptr;
-		}
+	virtual	void* GetSettings()
+	{
+		return nullptr;
+	}
 };
